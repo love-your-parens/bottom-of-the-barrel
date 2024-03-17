78ca2c09ea56ca8ac432163abe0ee01bc66a1551
Konrad Wątor <konrad@wator.it>
2024-03-17 15:42:18 +0100

    Add source: Manggha

---
+++ src/bottom_of_the_barrel/sources/manggha.clj
@@ -0,0 +1,81 @@
+(ns bottom-of-the-barrel.sources.manggha
+  "Scapes all exhibitions currently advertised by Manggha museum."
+  (:require [net.cgrand.enlive-html :as h]
+            [clojure.string :as s]))
+
+(def root-url "https://manggha.pl")
+
+(defn absolute-url
+  [relative-url]
+  (str root-url relative-url))
+
+(def seeds (map absolute-url
+                ["/wystawy-czasowe"]))
+
+(defn url->URL
+  [url]
+  (java.net.URL. url))
+
+(defn get-exhibitions-on-page
+  "Scrapes the provided page for all exhibition nodes.
+   Each is then scraped for relevant topics, e.g. exhibition title.
+   Note that there may be multiple matching child nodes per topic."
+  [page]
+  (let [p (h/html-resource page)]
+    (map (fn [a b] (cons a b))
+         (h/select p [:a.item]) ;url
+         (for [container (h/select p [:div.content-bottom])]
+           (for [selector [[:header :h4] ;title
+                           [:div.paragraph-text] ;description
+                           [:figure :img] ;thumbnail
+                           [:span.date] ;dates
+                           ]]
+             (first (h/select container selector)))))))
+
+(comment
+  (get-exhibitions-on-page (url->URL (first seeds))))
+
+(defn get-exhibitions
+  "Retrieves all exhibitions from all known pages."
+  []
+  (reduce into
+          (map get-exhibitions-on-page
+               (map url->URL seeds))))
+
+(comment
+  (get-exhibitions))
+
+(defn parse-date-string
+  "Converts a date string to a range of java.time.LocalDate objects."
+  [date]
+  (let [rs (take 2 (re-seq #"[0-9]+\.[0-9]+\.[0-9]+"
+                           (s/replace date "\n" "")))]
+    (for [r rs]
+      (when r
+        (let [[day month year] (map #(Integer/parseInt %) (s/split r #"\."))]
+          (java.time.LocalDate/of year month day))))))
+
+(comment
+  (parse-date-string "26.11.2023 - 05.05.2024"))
+
+(defn exhibition->map
+  "Converts a raw exhibition container into a structured map."
+  [[url title description thumbnail dates]]
+  {:url (absolute-url (get-in url [:attrs :href]))
+   :thumbnail (get-in thumbnail [:attrs :src])
+   :name (-> title h/text s/trim)
+   :description (-> description h/text s/trim)
+   :date (parse-date-string (h/text dates))
+   :place "Manggha"
+   :address "ul. M. Konopnickiej 26 30-302 Kraków"})
+
+(defn exhibitions->maps
+  [exhibitions]
+  (map exhibition->map exhibitions))
+
+(defn fetch []
+  (exhibitions->maps
+   (get-exhibitions)))
+
+(comment
+  (tap> (fetch)))
